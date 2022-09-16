4ca6e472c3cba05ec4a2b4cfbe59cd6b04b3d275
Charlotte Natasha <natashaonyango97@gmail.com>
2022-09-16 20:30:21 +0200

    styled navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import css from '../styles/Navbar.module.css'
 import Image from 'next/image'
 import Logo from '../assets/k-logo.png'
+import {UilShoppingBag} from '@iconscout/react-unicons'
 
 const Navbar = () => {
     return (
         <div className={css.navbar}>
             <div className={css.logo}>
                 <Image src={Logo} alt='' width={100} height={100} />
+                <span>Kirunyus Kitchen</span>
             </div>
 
             <ul className={css.menu}>
@@ -18,8 +20,11 @@ const Navbar = () => {
             </ul>
 
             {/* cart */}
-            <div className={css.cart}>
-                cart
+            <div className={css.cartSide}>
+                <div className={css.cart}>
+                    <UilShoppingBag size={35} color='#000'/>
+                    <div className={css.badge}>1</div>
+                </div>
             </div>
         </div>
     )
