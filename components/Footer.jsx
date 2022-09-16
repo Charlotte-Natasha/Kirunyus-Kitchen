b26340c874461c478b06c88e292dc01020624e88
Charlotte Natasha <natashaonyango97@gmail.com>
2022-09-16 20:44:53 +0200

    added logo to footer details

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import css from '../styles/Footer.module.css'
 import {UilFacebook, UilGithub, UilInstagram} from '@iconscout/react-unicons'
+import Image from 'next/image'
+import Logo from '../assets/k-logo.png'
 
 const Footer = () => {
     return (
         <div className={css.footer}>
-            <span></span>
+            <a href="">Created by</a>
 
             <div className={css.socials}>
                 <UilFacebook size={35}/>
                 <UilGithub size={35}/>
                 <UilInstagram size={35}/>
+            </div>
+
+            <div className={css.logo}>
+                <Image src={Logo} alt='' width={100} height={100} />
+                <span>Kirunyus Kitchen</span>
             </div>
         </div>
     )
