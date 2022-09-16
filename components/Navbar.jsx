e48f53241f27c092a96d87d49820871ccaf4f7ec
Charlotte Natasha <natashaonyango97@gmail.com>
2022-09-16 18:53:27 +0200

    added nav details

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import css from '../styles/Navbar.module.css'
+import Image from 'next/image'
+import Logo from '../assets/k-logo.png'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className={css.navbar}>
+            <div className={css.logo}>
+                <Image src={Logo} alt='' width={100} height={100} />
+            </div>
+
+            <ul className={css.menu}>
+                <li>Home</li>
+                <li>About</li>
+                <li>Gallery</li>
+                <li>Contacts</li>
+            </ul>
+
+            {/* cart */}
+            <div className={css.cart}>
+                cart
+            </div>
+        </div>
     )
 }
 
