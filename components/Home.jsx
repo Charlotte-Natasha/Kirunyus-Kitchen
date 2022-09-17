5b4ebab972305d8dffb7b4c8d6e40bef602b9152
Charlotte Natasha <natashaonyango97@gmail.com>
2022-09-17 11:50:57 +0200

    added home file and home css file

---
+++ components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from '../styles/Home.module.css'
+
+const Home = () => {
+    return (
+        <div className={css.home}>
+
+        </div>
+    )
+}
+
+export default Home
